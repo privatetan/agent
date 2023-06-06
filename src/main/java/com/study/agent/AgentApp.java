6619e25a0c1e9454e412a9b1a31b51a6c37cc32c
privatetan <privatetantao@gmail.com>
2023-06-06 13:50:33 +0800

    agent agentmain

--- src/main/java/com/study/agent/AgentApp.java
+++ src/main/java/com/study/agent/AgentApp.java
@@ -3,6 +3,7 @@ package com.study.agent;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
 import java.security.ProtectionDomain;
 
 import javassist.ClassPool;
@@ -17,23 +18,47 @@ public class AgentApp {
     /**
      * 静态加载
      * 
-     * @param agentOps
+     * @param args
      * @param inst
      */
-    public static void premain(String agentOps, Instrumentation inst) {
-        System.out.println("==enter premain==");
-        System.out.println(agentOps);
+    public static void premain(String args, Instrumentation inst) {
+        System.out.println("premain start");
+        System.out.println(args);
         inst.addTransformer(new AgentInstrumentation());
     }
 
     /**
      * 动态加载
      *
-     * @param agentOps
+     * @param args
      * @param inst
      */
-    public static void agentmain(String agentOps, Instrumentation inst) {
-        System.out.println(" ==enter agentmain:==");
+    public static void agentmain(String args, Instrumentation inst) {
+        System.out.println("agentmain start");
+        inst.addTransformer(new AgentInstrumentation());
+        Class[] allLoadedClasses = inst.getAllLoadedClasses();
+        if (allLoadedClasses != null){
+            for (Class<?> c: allLoadedClasses) {
+                if (c.isInterface() ||c.isAnnotation() ||c.isArray() ||c.isEnum()){
+                    continue;
+                }
+                if (c.getName().equals("com.study.agent.MainRun")) {
+                    try {
+                        System.out.println("retransformClasses start, class: " + c.getName());
+                        /*
+                         * retransformClasses()对JVM已经加载的类重新触发类加载。使用的就是上面注册的Transformer。
+                         * retransformClasses()可以修改方法体，但是不能变更方法签名、增加和删除方法/类的成员属性
+                         */
+                        inst.retransformClasses(c);
+                        System.out.println("retransformClasses end, class: " + c.getName());
+                    } catch (UnmodifiableClassException e) {
+                        System.out.println("retransformClasses error, class: " + c.getName() + ", ex:" + e);
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        System.out.println("agentmain end!");
     }
 
     /**
--- src/main/java/com/study/agent/MainRun.java
+++ src/main/java/com/study/agent/MainRun.java
@@ -2,14 +2,16 @@ package com.study.agent;
 
 /**
  * Agent增强目标类
- *
  */
-public class MainRun
-{
-    public static void main(String[] args) {
-        hello("world");
+public class MainRun {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("process result: " + hello("world"));
+            Thread.sleep(5000);
+        }
     }
-    private static void hello(String name) {
-        System.out.println("hello " + name);
+
+    private static String hello(String name) {
+        return "hello " + name;
     }
 }
